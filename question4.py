d40bb0989e1e5a3401889ed484c48bc141ef0141
athi9363 <athi5453@gmail.com>
2024-05-20 15:23:17 +0530

    done

--- question4.py
+++ question4.py
@@ -1,1 +1,10 @@
 #write a program to find the sum of digits of a given number'
+n=int(input("no:"))
+def sum(n):
+    total=0
+    while n>0:
+        total+=n%10
+        n//=10
+    return total
+
+print(sum(n))
